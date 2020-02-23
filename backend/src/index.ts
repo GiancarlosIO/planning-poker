3946c997c9b1e8d1d93d77075e841d924f1173cb
Giancarlos <giancarlos.isasi@gmail.com>
2020-02-22 20:14:45 -0500

    chore: Finish to configure docker and docker-compose

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,9 @@
-import { GraphQLServer } from 'graphql-yoga';
+import path from 'path';
+
+import * as GraphQLYoga from 'graphql-yoga';
+import express from 'express';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const PORT = 4000;
 
@@ -6,19 +11,55 @@ const typeDefs = `
   type Query {
     hello(name: String): String
   }
-`
+`;
 
 const resolvers = {
   Query: {
     hello: (_, { name }) => {
-      return `Hello ${name || 'hello'}!`
+      return `Hello ${name || 'hello'}!`;
     },
-  }
+  },
+};
+
+const server = new GraphQLYoga.GraphQLServer({ typeDefs, resolvers });
+const serverOptions = {
+  port: PORT,
+  endpoint: '/api/v1/graphql',
+  subscriptions: '/api/v1/subscriptions',
+  playground: '/api/v1/playground',
+  tracing: true,
+};
+
+if (isProduction) {
+  server.express.use(
+    express.static(path.join(__dirname, '../../frontend/dist'), {
+      extensions: ['js', 'css'],
+      index: false,
+    }),
+  );
+  server.express.get('*', (req, res, next) => {
+    if (
+      req.url === serverOptions.playground ||
+      req.url === serverOptions.subscriptions ||
+      req.url === serverOptions.endpoint
+    ) {
+      return next();
+    }
+    return res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+  });
+} else {
+  server.express.get('*', (req, res, next) => {
+    if (
+      req.url === serverOptions.playground ||
+      req.url === serverOptions.subscriptions ||
+      req.url === serverOptions.endpoint
+    ) {
+      return next();
+    }
+    res.redirect(serverOptions.playground);
+  });
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-
-server.start({ port: PORT, endpoint: '/api/v1/graphql', subscriptions: '/api/v1/subscriptions', playground: '/api/v1/playground' }, ({ port }) => {
+server.start(serverOptions, ({ port }) => {
   console.log(`> Server is running in http://localhost:${port}`);
 })
-
